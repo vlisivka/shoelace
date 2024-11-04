bcfbe2bc8a73d5c7fb5d3a213665f5261cccedc9
konnorrogers <konnor5456@gmail.com>
2024-11-04 13:34:43 -0500

    fix value not clearing with clear / tag removal

--- src/components/select/select.component.ts
+++ src/components/select/select.component.ts
@@ -464,6 +464,8 @@ export default class SlSelect extends ShoelaceElement implements ShoelaceFormCon
   private handleClearClick(event: MouseEvent) {
     event.stopPropagation();
 
+    this.valueHasChanged = true
+
     if (this.value !== '') {
       this.setSelectedOptions([]);
       this.displayInput.focus({ preventScroll: true });
@@ -534,6 +536,8 @@ export default class SlSelect extends ShoelaceElement implements ShoelaceFormCon
 
   private handleTagRemove(event: SlRemoveEvent, option: SlOption) {
     event.stopPropagation();
+
+    this.valueHasChanged = true
 
     if (!this.disabled) {
       this.toggleOptionSelection(option, false);
